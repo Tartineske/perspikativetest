702c28ab1a68d4d069d16ecf41d5f1462e3d448d
Perspikative <232093195+perspikative@users.noreply.github.com>
2026-06-13 22:27:43 +0200

    Add files via upload

--- script-comments.js
+++ script-comments.js
@@ -90,11 +90,9 @@
         '<div class="lb-comment-text">' + escapeHtml(comment.text) + '</div>' +
         (isOwn(comment)
           ? '<button class="lb-comment-delete" aria-label="Supprimer" title="Supprimer">' +
-              '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">' +
-                '<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>' +
-              '</svg>' +
+              '<img src="/icons/lightbox-trash.svg" alt="" aria-hidden="true">' +
             '</button>'
-          : '') +
+          : '')
       '</div>';
 
     // Listener suppression
